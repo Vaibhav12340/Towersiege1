a1406956e7346a8155c3a872e458b7c13855f95f
Vaibhav12340 <78683627+Vaibhav12340@users.noreply.github.com>
2021-02-07 16:05:14 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,12 @@
+ class Block{
+     constructor(x,y,width,height)
+
+     display(){
+         this.body.x=350;
+         this.body.y=200;
+         this.body.width=50;
+         this.body.height=50;
+     }
+     
+
+ }
---
+++ sketch.js
@@ -0,0 +1,14 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Body  = Matter.Body;
+
+function setup (){
+    CreateCanvas(800,400);
+    Hexagon = new hexagon
+
+}
+
+function draw(){
+Hexagon.display();
+Block.display();
+}
